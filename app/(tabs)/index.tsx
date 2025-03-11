6cc2009022479d6c5fe9c883ab2158fb2b8488d0
João Vitor Lima Mergulhão <jvitormergulho@gmail.com>
2025-03-10 23:59:46 -0300

    feat: add scrollList to home view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,77 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Link } from 'expo-router';
+import { StyleSheet, SafeAreaView, FlatList, Text, View } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const DATA = ['Recicle', 'Reutilize', 'Reduza', 'Recuse', 'Repense'];
 
 export default function HomeScreen() {
   return (
-    <ThemedView style={styles.titleContainer}>
-      <ThemedText>Hello, world!</ThemedText>
-    </ThemedView>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.text}>Hello, world!</Text>
+      </View>
+
+      <View style={styles.listsArea}>
+        <ScrollList DATA={DATA} />
+        <ScrollList DATA={DATA} />
+      </View>
+
+    </SafeAreaView>
   );
 }
 
+function ScrollList(
+  { DATA }: { DATA: string[] },
+) {
+  return (
+    <FlatList
+      data={DATA}
+      renderItem={({ item }) => (
+        <Link style={styles.item} href = "/service">
+          <Text style={styles.itemText}>{item}</Text>
+        </Link>
+      )}
+      keyExtractor={(item) => item}
+      horizontal={true} 
+      showsHorizontalScrollIndicator={false} 
+      contentContainerStyle={styles.listContainer}
+    />
+  )
+}
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 50,
+  },
   titleContainer: {
-    flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    marginBottom: 20,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  listContainer: {
+    paddingHorizontal: 10,
+  },
+  item: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: '#f0f0f0',
+    paddingVertical: 15,
+    height: 136,
+    width: 136,
+    paddingHorizontal: 20,
+    marginHorizontal: 10, // Adiciona espaçamento entre os itens
+    borderRadius: 8,
+  },
+  itemText: {
+    fontSize: 18,
+    color: '#555',
+  },
+  listsArea: {
+    gap: 20,
   },
 });
